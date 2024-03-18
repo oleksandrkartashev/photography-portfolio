848fdb13dd7f658fc135edf27db898322b63ee81
Oleksandr Kartashev <alexander.kartashev@mvfglobal.com>
2024-03-19 00:38:19 +0200

    update project path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/photography-portfolio/'
+    : '/',
   filenameHashing: false,
 });
